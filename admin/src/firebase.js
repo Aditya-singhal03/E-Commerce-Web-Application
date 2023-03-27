633dd0d5fa3b1e8c8d54f9afebae45cabc48e148
Adityasinghal03 <singhalbspr@gmail.com>
2023-03-27 20:43:36 +0530

    making folder available

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLwMl58oJGDcgB0khhhycCOCN7GP1b2fw",
+  authDomain: "shop-bd313.firebaseapp.com",
+  projectId: "shop-bd313",
+  storageBucket: "shop-bd313.appspot.com",
+  messagingSenderId: "1053013407752",
+  appId: "1:1053013407752:web:de1baf8e0829d6e70c39bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
